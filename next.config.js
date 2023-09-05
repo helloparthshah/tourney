e73dd10022fc660334e2d37e1ac51d0823a9b474
Parth Shah <helloparthshah@gmail.com>
2023-09-04 23:36:22 -0700

    Fixed bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        TEST_USER: process.env.TEST_USER,
+    },
+}
 
 module.exports = nextConfig

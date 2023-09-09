3ffe49f58777c13344bb817d2e357aa9518b6df9
Parth Shah <helloparthshah@gmail.com>
2023-09-09 16:39:11 -0700

    Added analytics

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Topbar from '@/components/topbar';
 import Footer from '@/components/footer';
 import { SessionProvider } from "next-auth/react"
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,6 +26,7 @@ export default function RootLayout({ children, session }) {
           </div>
           <Footer />
         </SessionProvider>
+        <Analytics />
       </body>
     </html>
   )

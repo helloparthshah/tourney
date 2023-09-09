4df6220288177784f20807fee1cc12e8b2e5920d
Parth Shah <helloparthshah@gmail.com>
2023-09-09 13:40:59 -0700

    Updated the battle winner algorithms

--- app/api/generate-winner/route.js
+++ app/api/generate-winner/route.js
@@ -7,18 +7,19 @@ export async function POST(request) {
     if (!session) return NextResponse.json({ error: 'Not authorized' }, { status: 401 });
     let { p1, p2 } = await request.json();
     // start both players with 100 health
-    // use willpower to multiply health by a factor of 10/(15-willpower)
-    let p1Health = 100 * (10 / (15 - p1.willpower));
-    let p2Health = 100 * (10 / (15 - p2.willpower));
+    // use willpower in the range of 0-10 to mke the health range 75 to 125
+
+    let p1Health = 75 + p1.willpower * 5;
+    let p2Health = 75 + p2.willpower * 5;
 
     // underdog bonus for the weaker player based on difference in all stats
     let p1Total = p1.strength + p1.speed + p1.luck + p1.intelligence + p1.willpower;
     let p2Total = p2.strength + p2.speed + p2.luck + p2.intelligence + p2.willpower;
-    if (p2Total > p1Total) {
+    /* if (p2Total > p1Total) {
         [p1Total, p2Total] = [p2Total, p1Total];
         [p1, p2] = [p2, p1];
-    }
-    let underdogFactor = 1 + Math.abs(p1Total - p2Total) / 60;
+    } */
+    let underdogFactor = 1 - Math.abs(p1Total - p2Total) / 60;
     let difference = Math.abs(p1Total - p2Total);
     const threshold = Math.min(p1Total, p2Total);
 
@@ -45,7 +46,7 @@ export async function POST(request) {
         p2.willpower *= underdogFactor;
     }
 
-    console.log(p1, p2);
+    // console.log(p1, p2);
     let isP1Damage0 = p1.strength == 0;
     let isP2Damage0 = p2.strength == 0;
 
@@ -55,23 +56,27 @@ export async function POST(request) {
     while (p1Health > 0 && p2Health > 0 && numRounds < MAX_ROUNDS) {
         numRounds++;
         if (isP1Damage0) {
-            p1.strength = Math.floor(Math.random() * 5);
+            p1.strength = Math.random() * 5;
         }
         if (isP2Damage0) {
-            p2.strength = Math.floor(Math.random() * 5);
+            p2.strength = Math.random() * 5;
         }
         // use speed to determine who goes first based on probability
-        let p1First = p1.speed / (p1.speed + p2.speed + 1) > Math.random();
-        if (p1First) {
-            p2Health -= p1.strength * (p1.luck / 10 > Math.random() ? 2 : 1) * (p1.intelligence / 10 > Math.random() ? 0.5 : 1);
+        let rollP1 = Math.random() * p1.speed;
+        let rollP2 = Math.random() * p2.speed;
+        let damageP1 = Math.random() * p1.strength;
+        let damageP2 = Math.random() * p2.strength;
+        // let p1First = p1.speed / (p1.speed + p2.speed + 1) > Math.random();
+        if (rollP1 > rollP2) {
+            p2Health -= damageP1 * (p1.luck / 10 > Math.random() ? 2 : 1) * (p2.intelligence / 10 > Math.random() ? 0.5 : 1);
             if (p2Health <= 0) break;
-            p1Health -= p2.strength * (p2.luck / 10 > Math.random() ? 2 : 1) * (p2.intelligence / 10 > Math.random() ? 0.5 : 1);
+            p1Health -= damageP2 * (p2.luck / 10 > Math.random() ? 2 : 1) * (p1.intelligence / 10 > Math.random() ? 0.5 : 1);
             if (p1Health <= 0) break;
         } else {
-            p2Health -= p1.strength * (p1.luck / 10 > Math.random() ? 2 : 1) * (p1.intelligence / 10 > Math.random() ? 0.5 : 1);
-            if (p2Health <= 0) break;
-            p1Health -= p2.strength * (p2.luck / 10 > Math.random() ? 2 : 1) * (p2.intelligence / 10 > Math.random() ? 0.5 : 1);
+            p1Health -= damageP2 * (p2.luck / 10 > Math.random() ? 2 : 1) * (p1.intelligence / 10 > Math.random() ? 0.5 : 1);
             if (p1Health <= 0) break;
+            p2Health -= damageP1 * (p1.luck / 10 > Math.random() ? 2 : 1) * (p2.intelligence / 10 > Math.random() ? 0.5 : 1);
+            if (p2Health <= 0) break;
         }
     }
     return NextResponse.json({
